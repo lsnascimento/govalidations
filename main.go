8f9d0c75eafbb52ad557ad49158c130f350a28eb
Leonardo Nascimento <lnascimento1988@gmail.com>
2022-06-17 01:31:47 -0300

    refactor: removal unused methods

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package validations
 
 import (
-	"reflect"
-
 	"github.com/lnascimento1988/govalidations/notifications"
 	"github.com/lnascimento1988/govalidations/validators"
 )
-
-type Parameters func(...interface{})
 
 type Validation struct {
 	Notifier   *notifications.Notifier
@@ -20,22 +16,5 @@ func New() Validation {
 	return Validation{
 		Notifier:   notifier,
 		Validators: validators.New(notifier),
-	}
-}
-
-func (validation Validation) All(args ...interface{}) {
-	for _, fn := range args {
-		switch reflect.TypeOf(fn).Kind() {
-		case reflect.Func:
-			reflect.ValueOf(fn).Call([]reflect.Value{})
-		default:
-			continue
-		}
-	}
-}
-
-func (validation Validation) All2(args ...Parameters) {
-	for _, fn := range args {
-		fn()
 	}
 }
